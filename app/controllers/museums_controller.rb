8c098458f8827e0563e9720ee196c6c1a8b9fc9b
Katherine Wise <k06kw02@gmail.com>
2021-09-16 14:43:51 +0100

    as far as I got wtih the test

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -3,11 +3,17 @@ class MuseumsController < ApplicationController
   require "rest-client"
 
   def get_museums
-    url
-
+    url = 'https://api.mapbox.com/places?access_token=#{MAPBOX_API_KEY}'
+    response = RestClient.get(url)
+    render json: response
   end
+
   def index
-    Museum.all
+     if params[:query].present?
+      @museum = Museum.where(latitude: params[:query], longitude: params[:query] )
+    else
+      @museum = museum.all
+    end
 
   end
 end
